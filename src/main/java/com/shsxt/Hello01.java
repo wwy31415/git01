e1ab405c9619eacad1b828d5f76a2224307d0f42
wwy31415 <2913537580@qq.com>
2018-10-25 00:21:52 +0800

    git01 Hello01.java

--- src/main/java/com/shsxt/Hello01.java
+++ src/main/java/com/shsxt/Hello01.java
@@ -2,8 +2,8 @@ package com.shsxt;
 
 public class Hello01 {
     public static void main(String[] args) {
-        System.out.println("Hello git02!");
-        System.out.println("Hello git01!");
+        System.out.println("Hello git04!");
+        System.out.println("Hello git03!");
         System.out.println("Hello!");
     }
 }
